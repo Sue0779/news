a4d43d8992867b77e21b91191720f21b9fa0b1ab
FedoraSu <tomekdziedzic79@gmail.com>
2025-03-02 13:29:39 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+# Konfiguracja serwera SMTP
+SMTP_HOST = "twój_host_smtp"
+SMTP_PORT = 465  # zazwyczaj 465 dla SSL
+SMTP_USER = "twój_adres_email"
+SMTP_PASSWORD = "twoje_hasło"
+
+# Adresy e-mail
+EMAIL_FROM = "twój_adres_email"
+EMAIL_TO = "adres_email_odbiorcy"
